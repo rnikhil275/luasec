ce54c5182a794124018ec4ff33e250374c60b9d6
Nikhil. R <rnikhil96@outlook.com>
2017-06-04 01:46:41 +0530

    add test code for testing CONNECT proxy

---
+++ test1.lua
@@ -0,0 +1,24 @@
+local https = require("ssl.https")
+
+local function doreq(url)
+	local reqt = {
+	  url = url,
+	  -- redirect = 'all',     --> allows https-> http redirect
+	  target = {},
+	  proxy = "http://172.28.128.1:4444",
+	  connectproxy = 'true',
+	}
+	reqt.sink = ltn12.sink.table(reqt.target)
+
+	local result, code, headers, status = https.request(reqt)
+	print("Fetching:",url,"==>",code, status)
+	-- if headers then for k,v in pairs(headers) do print("",k,v) end end
+	-- print(result)
+
+	return result, code, headers, status
+end
+
+local result, code, headers, status = doreq("https://icanhazip.com/")
+
+
+
